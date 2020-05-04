bf822c2185682981084d4aaba123b396d419cc98
Yasin Hatibi <yhatibi@gmail.com>
2020-05-04 02:16:40 +0200

    Clase CartaRandom no genera una carta random entre 12 cartas.

--- src/com/company/CartaRandom.java
+++ src/com/company/CartaRandom.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class CartaRandom {
 
-    String carta [] = { "1","2","3","4","5","6","7","8","9","10","10","10"};
+    int carta [] = { 1,2,3,4,5,6,7,8,9,10,10,10};
  
-    public void GenerarCarta() {
+    public int GenerarCarta() {
         //Generar una Carta [A=1, J=10, Q=10, K=10]
 
         Random random = new Random();
         int x = random.nextInt(12);
-        System.out.println(carta[x]);
+        int y = (carta[x]);
 
-
+        return y;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,44 @@
 package com.company;
 
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
+        Scanner scanner = new Scanner(System.in);
         CartaRandom cartaRandom = new CartaRandom();
 
-        cartaRandom.GenerarCarta();
 
+        int carta1Dealer = cartaRandom.GenerarCarta();
+        int carta2Dealer = cartaRandom.GenerarCarta();
+        int sumaDealer = carta1Dealer + carta2Dealer;
+
+        int carta1Jugador = cartaRandom.GenerarCarta();
+        int carta2Jugador = cartaRandom.GenerarCarta();
+        int sumaJugador = carta1Jugador + carta2Jugador;
+
+        System.out.println("Cartas del Dealer: ");
+        System.out.println(carta1Dealer+","+carta2Dealer+ " ("+sumaDealer+")");
+
+        System.out.println("Tus cartas:");
+        System.out.println(carta1Jugador+","+carta2Jugador+ " ("+sumaJugador+")");
+
+
+
+        System.out.println();
+        System.out.println("Pedir= true | Parar = false");
+        Boolean pedir = scanner.nextBoolean();
+
+
+        while (pedir == true) {
+            System.out.println("estas dentro del bucle");
+            pedir = scanner.nextBoolean();
+        }
+
+
+
+        System.out.println("Final");
     }
 }
